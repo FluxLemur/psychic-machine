e7c2a687d9aacedd2eb5c12f5121147ec0da3fd0
Leo Mehr <ljm262@cornell.edu>
2014-12-24 22:20:59 +0200

    authors

--- src/main.py
+++ src/main.py
@@ -3,13 +3,12 @@ from textLearner import TextLearner
 
 DATA_DIR = '../data/'
 
-dat = open(os.path.join(DATA_DIR, 'simple.txt'))
-simpleLearner = TextLearner('test data')
+dat = open(os.path.join(DATA_DIR, 'lewis_carroll.txt'))
+simpleLearner = TextLearner('carroll')
 long_line = ''
 for line in dat:
     long_line += line
 
 simpleLearner.learn(long_line)
 
-#simpleLearner.print_mat()
-print simpleLearner.write(50)
+print simpleLearner.write(10)
